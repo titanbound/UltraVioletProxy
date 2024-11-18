520f3215b5ee10d47b4a24d947e362c894c4e32e
NateTheGreat09 <eniteoluwa@icloud.com>
2024-11-18 16:23:38 -0600

    Update server.js

--- server.js
+++ server.js
@@ -9,6 +9,37 @@ import { createServer } from "node:http";
 import { join } from "node:path";
 import { fileURLToPath } from "node:url";
 
+const connection = new BareMux.BareMuxConnection("/baremux/worker.js")
+const wispUrl = (location.protocol === "https:" ? "wss" : "ws") + "://" + location.host + "/wisp/";
+const bareUrl = (location.protocol === "https:" ? "https" : "http") + "://" + location.host + "/bare/"
+document // makes it so you can press enter to submit as opposed to just being able to press a button
+    .getElementById("urlInput")
+    .addEventListener("keydown", function (event) {
+        if (event.key === "Enter") {
+            event.preventDefault();
+            document.getElementById("searchButton").click();
+        }
+    });
+
+document.getElementById("searchButton").onclick = async function (event) {
+    event.preventDefault();
+
+    let url = document.getElementById("urlInput").value; // if no periods are detected in the input, search google instead
+    let searchUrl = "https://www.google.com/search?q=";
+
+    if (!url.includes(".")) {
+        url = searchUrl + encodeURIComponent(url);
+    } else {
+        if (!url.startsWith("http://") && !url.startsWith("https://")) { // if no http or https is detected, add https automatically
+            url = "https://" + url;
+        }
+    }
+	if (!await connection.getTransport()) {
+		await connection.setTransport("/epoxy/index.mjs", [{ wisp: wispUrl }]);
+	}
+    iframeWindow.src = __uv$config.prefix + __uv$config.encodeUrl(url);
+};
+
 const bare = createBareServer("/bare/")
 const __dirname = join(fileURLToPath(import.meta.url), "..");
 const app = express();
